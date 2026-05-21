f44e35032ade244850637f2d1cb03230cc3e4b82
abraham-peter <Peter.St.Abraham@student.utcluj.ro>
2026-05-21 17:28:22 +0300

    alembic and docker added

--- backend/db.py
+++ backend/db.py
@@ -1,25 +1,25 @@
-# from sqlmodel import Field,SQLModel,create_engine
+from sqlmodel import Field,SQLModel,create_engine
 
-# class BankConnection(SQLModel, table=True):
-#     id: int = Field(default=None, primary_key=True)
-#     user_id: int = Field(foreign_key="user.id")
-#     bank_name: str
-#     access_token: str
-# class Subscription(SQLModel, table=True):
-#     id: int = Field(default=None, primary_key=True)
-#     user_id: int = Field(foreign_key="user.id")
-#     merchant_name: str
-#     amount: float
-#     next_billing_date: str
-#     status: str
+class BankConnection(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    user_id: int = Field(foreign_key="user.id")
+    bank_name: str
+    access_token: str
+class Subscription(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    user_id: int = Field(foreign_key="user.id")
+    merchant_name: str
+    amount: float
+    next_billing_date: str
+    status: str
 
-# sqlite_file_name = "database.db"
-# sqlite_url = f"sqlite:///{sqlite_file_name}"
+postgres_file_name = "database.db"
+postgres_url = "postgresql://myuser:mypassword@localhost:5432/mydb"
 
-# engine = create_engine(sqlite_url, echo=True)
+engine = create_engine(postgres_url, echo=True)
 
-# def create_db_and_tables():
-#     SQLModel.metadata.create_all(engine)
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
 
-# if __name__ == "__main__":
-#     create_db_and_tables()
+if __name__ == "__main__":
+    create_db_and_tables()
